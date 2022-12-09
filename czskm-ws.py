c5310f7542b534ed5f4d2d6abde20a8a14e70f79
WafuRuns <remiliass@gmail.com>
2022-12-09 02:32:16 +0100

    Update czskm-ws.py

--- czskm-ws.py
+++ czskm-ws.py
@@ -2,50 +2,75 @@ import asyncio
 import simpleobsws
 import requests
 
-loop = asyncio.get_event_loop()
-ws = simpleobsws.obsws(host='host.docker.internal', port=4444, password='', loop=loop)
-key = ''
-ip = ''
+parameters = simpleobsws.IdentificationParameters(ignoreNonFatalRequestChecks = False)
+ws = simpleobsws.WebSocketClient(
+    url='ws://host.docker.internal:4455',
+    password='###',
+    identification_parameters=parameters
+)
+key = '###'
+ip = '###'
 
 async def switch_layout():
     await ws.connect()
+    await ws.wait_until_identified()
+
     old_layout = None
     old_rtmp = None
     while True:
-        itemlist = await ws.call('GetSceneItemList')
-        itemlist = itemlist['sceneItems']
-        request = requests.get(f'http://{ip}/nodecg-czskm/ws', {'key': key}).json()
+        itemlist_request = simpleobsws.Request('GetSceneItemList', {'sceneName': 'Speedcontrol'})
+        itemlist = await ws.call(itemlist_request)
+        if itemlist.ok():
+            itemlist = itemlist.responseData['sceneItems']
+        request = requests.get(f'https://{ip}/nodecg-czskm/ws', {'key': key}).json()
         current_layout = request['layout']
         rtmp_settings = request['rtmp']
+        print(rtmp_settings)
         if current_layout:
             current_layout = current_layout.split('.')[0]
             if current_layout != old_layout:
                 await asyncio.sleep(0.5)
                 for item in itemlist:
                     if item['sourceName'] == current_layout:
-                        await ws.call('SetSceneItemRender', data={
-                            'scene-name': 'Speedcontrol',
-                            'source': item['sourceName'],
-                            'render': True
+                        request = simpleobsws.Request('SetSceneItemEnabled', {
+                            'sceneName': 'Speedcontrol',
+                            'sceneItemId': item['sceneItemId'],
+                            'sceneItemEnabled': True
                         })
+                        await ws.call(request)
                         continue
-                    if item['sourceName'] not in ['Background', 'Layout', 'Donationtotal']:
-                        await ws.call('SetSceneItemRender', data={
-                            'scene-name': 'Speedcontrol',
-                            'source': item['sourceName'],
-                            'render': False
+                    if item['sourceName'] == 'Snow' and current_layout in ['continue', 'end', 'start', 'intermission']:
+                        request = simpleobsws.Request('SetSceneItemEnabled', {
+                            'sceneName': 'Speedcontrol',
+                            'sceneItemId': item['sceneItemId'],
+                            'sceneItemEnabled': True
                         })
+                        await ws.call(request)
+                        continue
+                    if item['sourceName'] not in ['Background', 'Graphics', 'Donationtotal', 'Donation']:
+                        request = simpleobsws.Request('SetSceneItemEnabled', {
+                            'sceneName': 'Speedcontrol',
+                            'sceneItemId': item['sceneItemId'],
+                            'sceneItemEnabled': False
+                        })
+                        await ws.call(request)
                 old_layout = current_layout
         if rtmp_settings:
             if rtmp_settings != old_rtmp:
+                rtmp_request = simpleobsws.Request('GetInputList')
+                inputs = await ws.call(rtmp_request)
+                if inputs.ok():
+                    inputs = inputs.responseData['inputs']
                 split_settings = rtmp_settings.split('|')
-                await ws.call('SetSourceSettings', data={
-                    'sourceName': split_settings[0],
-                    'sourceSettings': {
-                        'input': split_settings[1]
-                    }
-                })
+                for input in inputs:
+                    if input['inputName'] == split_settings[0]:
+                        settings_request = simpleobsws.Request('SetInputSettings', {
+                            'inputName': input['inputName'],
+                            'inputSettings': {'input': split_settings[1]}
+                        })
+                        await ws.call(settings_request)
                 old_rtmp = rtmp_settings
         await asyncio.sleep(0.2)
 
+loop = asyncio.get_event_loop()
 loop.run_until_complete(switch_layout())
