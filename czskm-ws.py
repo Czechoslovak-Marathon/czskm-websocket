c86e8d0d66ce4f9634b33f1a4290aa1db7d32851
WafuRuns <remiliass@gmail.com>
2023-08-06 18:59:24 +0200

    Update czskm-ws.py

--- czskm-ws.py
+++ czskm-ws.py
@@ -10,6 +10,7 @@ ws = simpleobsws.WebSocketClient(
 )
 key = '###'
 ip = '###'
+music_url = 'http://host.docker.internal:8000'
 
 async def switch_layout():
     await ws.connect()
@@ -17,6 +18,7 @@ async def switch_layout():
 
     old_layout = None
     old_rtmp = None
+    old_music = None
     while True:
         itemlist_request = simpleobsws.Request('GetSceneItemList', {'sceneName': 'Speedcontrol'})
         itemlist = await ws.call(itemlist_request)
@@ -25,6 +27,7 @@ async def switch_layout():
         request = requests.get(f'https://{ip}/nodecg-czskm/ws', {'key': key}).json()
         current_layout = request['layout']
         rtmp_settings = request['rtmp']
+        music = request['music']
         if current_layout:
             current_layout = current_layout.split('.')[0]
             if current_layout != old_layout:
@@ -53,12 +56,13 @@ async def switch_layout():
                             'sceneItemEnabled': False
                         })
                         await ws.call(request)
-                old_layout = current_layout
                 if current_layout not in ['continue', 'end', 'start', 'intermission']:
                     request = simpleobsws.Request('StartRecord')
                 else:
                     request = simpleobsws.Request('StopRecord')
+                    requests.get(music_url)
                 await ws.call(request)
+                old_layout = current_layout
         if rtmp_settings:
             if rtmp_settings != old_rtmp:
                 rtmp_request = simpleobsws.Request('GetInputList')
@@ -74,6 +78,10 @@ async def switch_layout():
                         })
                         await ws.call(settings_request)
                 old_rtmp = rtmp_settings
+        if music:
+            if music != old_music:
+                requests.get(music_url)
+                old_music = music
         await asyncio.sleep(0.2)
 
 loop = asyncio.get_event_loop()
